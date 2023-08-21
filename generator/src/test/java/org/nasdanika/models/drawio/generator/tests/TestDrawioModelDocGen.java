beceae9265f668146e7de88b41496b7f18fc8502
Pavel <Pavel.Vlasov@nasdanika.org>
2023-08-21 11:50:24 -0400

    Sitemap fix

--- generator/src/test/java/org/nasdanika/models/drawio/generator/tests/TestDrawioModelDocGen.java
+++ generator/src/test/java/org/nasdanika/models/drawio/generator/tests/TestDrawioModelDocGen.java
@@ -170,7 +170,7 @@ public class TestDrawioModelDocGen {
 		String pageTemplateResource = "page-template.yml";
 		URI pageTemplateURI = URI.createFileURI(new File(pageTemplateResource).getAbsolutePath());//.appendFragment("/");
 		
-		String siteMapDomain = "https://gitlab.models.nasdanika.org";		
+		String siteMapDomain = "https://drawio.models.nasdanika.org";		
 		ActionSiteGenerator actionSiteGenerator = new ActionSiteGenerator() {
 			
 			protected boolean isDeleteOutputPath(String path) {
